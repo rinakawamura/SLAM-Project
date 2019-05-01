7b4aa0f65cd7535dc935ffde75c8b2822b4cf2b6
Harshal Sheth (on car) <harshal.sheth@yale.edu>
2019-04-30 20:04:00 -0400

    tweaks on segmentation

--- src/segmentation.py
+++ src/segmentation.py
@@ -19,7 +19,7 @@ def map_callback(data):
 	height = data.info.height
 	res = data.info.resolution
 
-	m = np.array(data.data).reshape((height, width))  # TODO check this
+	m = np.array(data.data).reshape((height, width))
 	rospy.loginfo(np.shape(m))
 	
 	obstacles = m >= 90
@@ -29,13 +29,13 @@ def map_callback(data):
 	obs_count = np.shape(obs_loc)[1]
 
 	nearby = []
-	threshold = 0.1
+	threshold = 0.05
 	for i in range(obs_count):
 		for j in range(i+1, obs_count):
 			x1, y1 = obs_loc[0][i], obs_loc[1][i]
 			x2, y2 = obs_loc[0][j], obs_loc[1][j]
 			if (x1-x2)**2 + (y1-y2)**2 < threshold/(res**2):
-				nearby.append((x1,y1,x2,y2))
+				nearby.append((i, j))
 	rospy.loginfo(len(nearby))
 
 	draw = Marker()
@@ -43,18 +43,16 @@ def map_callback(data):
 	draw.ns = ns
 	draw.id = id_no
 	draw.type = Marker.LINE_LIST
-	print(data.info.origin)
 	draw.pose = data.info.origin
-	draw.pose.orientation = Quaternion(0, 0, 0, 1)
-	draw.scale = Vector3(0.05, 0, 0)
+	draw.scale = Vector3(0.03, 0, 0)
 	draw.lifetime = rospy.Duration(5)
 	draw.frame_locked = False
 	points = []
 	colors = []
 	for pair in nearby:
-		pair = list(reversed(pair))
-		points.append(Point(res*pair[0], res*pair[1], 0))
-		points.append(Point(res*pair[2], res*pair[3], 0))
+		i, j = pair
+		points.append(Point(res*obs_loc[1][i], res*obs_loc[0][i], 0))
+		points.append(Point(res*obs_loc[1][j], res*obs_loc[0][j], 0))
 		color = ColorRGBA(0, 0, 1, 0.7)
 		colors.append(color)
 		colors.append(color)
@@ -71,7 +69,7 @@ def listener():
 	rospy.Subscriber('/map', OccupancyGrid, map_callback)
 	walls = rospy.Publisher('/walls', Marker, queue_size=1)
 
-	frequency = 10  # Hz
+	frequency = 3  # Hz
 	rate = rospy.Rate(frequency)
 	while not rospy.is_shutdown():
 		#rospy.loginfo("inside loop")
